2a8cbc29a92f284a3a9aff9c95c98bf535f5a620
boogiedk <ganz161@mail.ru>
2018-12-22 01:10:41 +0300

    added navmenu in project

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { MatMenuModule} from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ParityComponent } from './components/parityOfTheWeek/parityOfTheWeek.component';
+import {MenuOverview} from './components/navmenu/navmenu.component'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     ParityComponent,
+    MenuOverview
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
+  entryComponents: [MenuOverview],
   providers: [],
-  bootstrap: [ParityComponent]
+  bootstrap: [
+    ParityComponent,
+    MenuOverview
+  ]
 })
 export class AppModule { }
