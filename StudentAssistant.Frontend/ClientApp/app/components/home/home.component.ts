10a6455b23c768d474d61b7772f6112830ee1f64
Dmitry Kulkin <d.kulkin@iml.ru>
2019-01-30 18:05:25 +0300

    form sendfeedback is working

--- StudentAssistant.Frontend/ClientApp/app/components/home/home.component.ts
+++ StudentAssistant.Frontend/ClientApp/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'home',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    _http: Http;
+
+    public constructor(http: Http) {
+        this._http = http;
+    }
+
+    onSubmit(userFeedbackRequestModel: UserFeedbackRequestModel) {
+        this._http.post('http://localhost:18936/api/support/sendfeedback', userFeedbackRequestModel).subscribe(status => console.log(JSON.stringify(status)));
+    }
 }
+
+export interface UserFeedbackRequestModel {
+
+    userName: string;
+    emailTo: string;
+    subject: string;
+    textBody: string;
+}
+
+
+
