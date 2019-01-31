85ce450079a0e07a720c7c051ce549326247546e
Dmitry Kulkin <d.kulkin@iml.ru>
2019-01-31 17:00:08 +0300

    added ErrorMessage in attribute's UserFeedbackRequestModel

--- StudentAssistant.Backend/Models/UserSupport/UserFeedbackRequestModel.cs
+++ StudentAssistant.Backend/Models/UserSupport/UserFeedbackRequestModel.cs
@@ -12,29 +12,29 @@ namespace StudentAssistant.Backend.Models.UserSupport
         /// Имя пользователя.
         /// </summary>
         [Required(ErrorMessage = "Не указано имя пользователя")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "Превышен размер длины имени пользователя")]
         public string UserName { get; set; }
 
         /// <summary>
         /// Почта пользователя.
         /// </summary>
         [Required(ErrorMessage = "Не указан почтовый адрес")]
-        [EmailAddress(ErrorMessage = "Некорректный адрес")]
-        [StringLength(50)]
+        [EmailAddress(ErrorMessage = "Некорректный почтовый адрес")]
+        [StringLength(50,ErrorMessage = "Превышен размер длины почтового адреса")]
         public string EmailTo {get;set;}
 
         /// <summary>
         /// Тема сообщения.
         /// </summary>
         [Required(ErrorMessage = "Не указана тема сообщения")]
-        [StringLength(50)]
+        [StringLength(50,ErrorMessage = "Превышен размер длины темы сообщения")]
         public string Subject { get; set; }
 
         /// <summary>
         /// Текст сообщения.
         /// </summary>
         [Required(ErrorMessage = "Не указан текст сообщения")]
-        [StringLength(500)]
+        [StringLength(500, ErrorMessage = "Превышен размер длины текста сообщения")]
         public string TextBody { get; set; }
     }
 }
