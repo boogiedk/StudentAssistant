8e90b8bc94ac2fba6d53de4f346b11604a68732a
Dmitry Kulkin <d.kulkin@iml.ru>
2019-01-31 17:55:18 +0300

    upgraded validation service

--- StudentAssistant.Backend/Services/Implementation/ValidationService.cs
+++ StudentAssistant.Backend/Services/Implementation/ValidationService.cs
@@ -4,32 +4,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace StudentAssistant.Backend.Services.Implementation
 {
     public class ValidationService : IValidationService
     {
-        public List<ValidationResultModel> ValidateRequest(UserFeedbackRequestModel input)
+        public List<ValidationResultModel> ValidateRequest(IEnumerable<ModelError> input)
         {
             List<ValidationResultModel> errorList = new List<ValidationResultModel>();
 
             if (input == null)
-                errorList.Add(new ValidationResultModel() { ErrorMessage = "Запрос не содержит данных." });
+                errorList.Add(new ValidationResultModel { ErrorMessage = "Запрос не содержит данных." });
 
-            //TODO:
-            //проверить, что быстрее, equals, nullOrEmpty или input==null
-            if (string.IsNullOrEmpty(input.EmailTo))
-                errorList.Add(new ValidationResultModel() { ErrorMessage = "Запрос не содержит E-mail." });
+            // создать общую модель ошибки для ответа клиенту
 
-            if (string.IsNullOrEmpty(input.TextBody))
-                errorList.Add(new ValidationResultModel() { ErrorMessage = "Запрос не содержит текста." });
+            return errorList;
+        }
 
-            if (string.IsNullOrEmpty(input.Subject))
-                errorList.Add(new ValidationResultModel() { ErrorMessage = "Запрос не содержит заголовка." });
+        public List<ValidationResultModel> PrepareErrorResult(UserFeedbackResultModel input)
+        {
+            List<ValidationResultModel> errorList = new List<ValidationResultModel>();
 
-            if (string.IsNullOrEmpty(input.UserName))
-                errorList.Add(new ValidationResultModel() { ErrorMessage = "Запрос не содержит имени пользователя." });
+            if (input == null)
+                errorList.Add(new ValidationResultModel { ErrorMessage = "Запрос не содержит данных." });
 
+            // создать общую модель ошибки для ответа клиенту
 
             return errorList;
         }
